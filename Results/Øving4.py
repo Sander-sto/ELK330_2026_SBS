2cc6cdcc4cd496c8a87e3574869af6d52734fc24
Sander Bolstad Stokkenes <sb.stokkenes@stud.uis.no>
2026-09-09 15:33:02 +0200

    oppg.7

--- Results/Øving4.py
+++ Results/Øving4.py
@@ -5,7 +5,7 @@ import time
 from datetime import datetime,timezone
 #oppggave 1
 df = pd.read_csv(
-    r"C:\Users\Sande\OneDrive - Universitetet i Stavanger\Documents\GitHub\power-system-data\load\load_data.csv",parse_dates=["Time(Local)"],decimal=","
+    r"load\load_data.csv",parse_dates=["Time(Local)"],decimal=","
 )
 df["Time(Local)"] = pd.to_datetime(df["Time(Local)"], format="%d.%m.%Y %H:%M:%S %z",utc=True).dt.tz_convert("Europe/Oslo")
 
@@ -15,6 +15,13 @@ df = df.set_index("Time(Local)")
 #print(df.loc["2026-01-01 03:00"])
 
 dags_profil=df.loc["2026-02-10"]
+
+df["Netto"] = (
+    df["Production"]
+    - df["Consumption"]
+)
+
+print(df.head(5))
 
 dags_profil.plot(
     y=["Consumption"],
